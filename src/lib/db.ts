d13527760ff181bdd8486d79c238d2ce7e300906
devpolas <195308698+devpolas@users.noreply.github.com>
2026-09-05 00:29:38 +0600

    refactor: change db location

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import postgres from "@prisma/orm-postgres/runtime";
-import type { Contract } from "./contract";
-import contractJson from "./contract.json" with { type: "json" };
+import type { Contract } from "./../../prisma/contract";
+import contractJson from "./../../prisma/contract.json" with { type: "json" };
 
 export const db = postgres<Contract>({
   contractJson,
