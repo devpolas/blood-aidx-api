ea14489043cd166aaf5da1f7523b2a3430953026
devpolas <195308698+devpolas@users.noreply.github.com>
2026-09-07 01:19:07 +0600

    implement: notification routes

--- src/app.ts
+++ src/app.ts
@@ -26,6 +26,7 @@ import organizationRouter from "./modules/organization/organization.route";
 import milestoneRouter from "./modules/milestone/milestone.route";
 import certificateRouter from "./modules/certificate/certificate.route";
 import reportRouter from "./modules/report/report.route";
+import notificationRouter from "./modules/notification/notification.route";
 
 const app: Application = express();
 
@@ -118,6 +119,7 @@ app.use("/api/v1/locations", locationRouter);
 app.use("/api/v1/milestones", milestoneRouter);
 app.use("/api/v1/certificates", certificateRouter);
 app.use("/api/v1/reports", reportRouter);
+app.use("/api/v1/notifications", notificationRouter);
 
 app.use(notFound);
 app.use(globalErrorController);
